a0c4bc7504a872071ef8ae05e3afe45edb0179df
Iarlen <iarlensr@hotmail.com>
2023-07-26 02:19:44 -0300

    feat: implementing the functionality of expenseController.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
 import 'dotenv/config'
 import { env } from './utils/envShema'
 import TelegramBot, { Message } from 'node-telegram-bot-api'
-import { UserController } from './controllers/useController'
+import { UserController } from './controllers/userController'
+import { CommandController } from './controllers/commandController'
+import { ExpenseController } from './controllers/expenseController'
 
 class Bot {
   private bot: TelegramBot
   private userController: UserController
+  private commandController: CommandController
+  private expenseController: ExpenseController
 
   constructor() {
     this.bot = new TelegramBot(env.TELEGRAM_TOKEN, { polling: true })
     this.userController = new UserController()
+    this.commandController = new CommandController()
+    this.expenseController = new ExpenseController()
 
     this.start()
   }
@@ -18,8 +24,56 @@ class Bot {
     this.bot.onText(/\/start/, (msg) => {
       const { username, sendMessage, userId } = this.getMessage(msg)
 
-      this.userController.register(userId.toString())
-      this.userController.wellcome(username, sendMessage)
+      this.userController.wellcome(userId, username, sendMessage)
+    })
+
+    this.bot.onText(/\/help/, (msg) => {
+      const { sendMessage } = this.getMessage(msg)
+
+      this.commandController.allCommands(sendMessage)
+    })
+
+    this.bot.onText(/\/add (.+)/, (msg, match) => {
+      const { userId, sendMessage } = this.getMessage(msg)
+
+      if (!match) {
+        return sendMessage('Por favor, envie no formato: /add uber 34.90.')
+      }
+
+      const [expenseName, expenseAmount] = match[1].split(' ')
+
+      if (!expenseName || !expenseAmount) {
+        return this.commandController.formatExpenseIvalid(sendMessage)
+      }
+
+      const expense = {
+        name: expenseName,
+        amount: expenseAmount,
+      }
+
+      this.expenseController.create(userId, expense, sendMessage)
+    })
+
+    this.bot.onText(/\/finances/, (msg) => {
+      const { userId, sendMessage } = this.getMessage(msg)
+
+      this.expenseController.getAllExpenses(userId, sendMessage)
+    })
+
+    this.bot.onText(/\/profile/, (msg) => {
+      const { userId, username, sendMessage } = this.getMessage(msg)
+
+      this.userController.profile(userId.toString(), username, sendMessage)
+    })
+
+    this.bot.onText(/\/comandos/, (msg) => {
+      const { userId } = this.getMessage(msg)
+
+      this.bot.sendMessage(userId, 'Listas dos comandos:', {
+        reply_markup: {
+          keyboard: [[{ text: '/help' }, { text: '/comandos' }]],
+        },
+      })
     })
   }
 
