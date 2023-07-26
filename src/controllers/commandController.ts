28a5d497a862a1941be794a0fb646a247f033bd5
Iarlen <iarlensr@hotmail.com>
2023-07-26 02:17:30 -0300

    feat: creating functionalities to build the expense message successfully created and to list all user expenses.

--- src/controllers/commandController.ts
+++ src/controllers/commandController.ts
@@ -1,5 +1,12 @@
 import { commands } from '../utils/commnands'
 
+interface IExpenseProps {
+  id: string
+  name: string
+  amount: string
+  createdAt: Date
+}
+
 export class CommandController {
   allCommands = (sendMessage: (text: string) => void) => {
     sendMessage(
@@ -7,6 +14,39 @@ export class CommandController {
         .map(
           (comand) =>
             `COMANDO: ${comand.command} \nDESCRIÇÃO: ${comand.description}\n`,
+        )
+        .join('\n')}`,
+    )
+  }
+
+  formatExpenseIvalid = (sendMessage: (text: string) => void) => {
+    sendMessage(`Envie no seguinte formato:\n/add Uber 19.96`)
+  }
+
+  expenseCreateWithSucess = (
+    expense: IExpenseProps,
+    sendMessage: (text: string) => void,
+  ) => {
+    sendMessage(
+      `Despesa criada com sucesso!\nID: ${expense.id}\nNOME: ${
+        expense.name
+      }\nVALOR: R$ ${
+        expense.amount
+      }\nCRIADO EM: ${expense.createdAt.toLocaleDateString()}`,
+    )
+  }
+
+  allExpenses = (
+    expenses: IExpenseProps[],
+    sendMessage: (text: string) => void,
+  ) => {
+    sendMessage(
+      `ESSES SÃO SUAS DESPESAS: \n${expenses
+        .map(
+          (expense) =>
+            `\nID: ${expense.id}\nNOME: ${expense.name}\nVALOR: R$ ${
+              expense.amount
+            }\nCRIADO EM: ${expense.createdAt.toLocaleDateString()}`,
         )
         .join('\n')}`,
     )
