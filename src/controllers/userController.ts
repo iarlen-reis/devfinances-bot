fcc121b90ab4f03859e23f052020789ed5cb4018
Iarlen <iarlensr@hotmail.com>
2023-07-26 00:57:17 -0300

    fix: correcting filename from useController to userController.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,10 +2,22 @@ import { prisma } from '../utils/prisma'
 
 export class UserController {
   wellcome = (
+    userId: string,
     username: string | undefined,
     sendMessage: (text: string) => void,
   ) => {
+    this.register(userId)
     sendMessage(`Olá ${username}, seja bem-vindo ao devFinances!`)
+  }
+
+  profile = (
+    userId: string,
+    username: string | undefined,
+    sendMessage: (text: string) => void,
+  ) => {
+    sendMessage(
+      `Olá ${username}, esses são seus dados: \nNOME: ${username} \nFINANÇAS MENSAL: 0 \nFINANÇAS TOTAL: 0`,
+    )
   }
 
   register = async (userId: string) => {
